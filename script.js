875fad6227654be4ba155e12b6e4cf29bf9c74b3
Nishant-codess <nishant.ranjan.air1@gmail.com>
2025-06-16 21:28:36 +0530

    Updated Script.js

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
+// Get nav elements and buttons
 const navbarLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
+
+// Open mobile menu
 menuOpenButton.addEventListener("click", () => {
-  // Toggle mobile menu visibility
   document.body.classList.toggle("show-mobile-menu");
 });
-// Close menu when the close button is clicked
+
+// Close menu on clicking close button
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
-// Close menu when nav link is clicked
+
+// Close menu when a nav link is clicked
 navbarLinks.forEach((link) => {
-  link.addEventListener("c
+  link.addEventListener("click", () => menuOpenButton.click());
+});
+
+/* Initializing Swiper */
 let swiper = new Swiper(".slider-wrapper", {
   loop: true,
   grabCursor: true,
   spaceBetween: 25,
-  // Pagination bullets
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    dynamicBullets: lick", () => menuOpenButton.click());
-});
-/* Initializing Swiper */true,
+    dynamicBullets: true,
   },
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  /* Responsive breakpoints */
   breakpoints: {
     0: {
       slidesPerView: 1,
